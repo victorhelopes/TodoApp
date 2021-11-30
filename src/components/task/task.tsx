188dd9f994a7352aa65be99913183ec834069fea
victorhelopes <victorhelopes@gmail.com>
2021-11-30 17:28:28 -0300

    CRUD task and added styles

--- src/components/task/task.tsx
+++ src/components/task/task.tsx
@@ -1,15 +1,27 @@
+import { useState } from 'react'
 import './styles.css'
 
-export default function task(props: any){
-    function oi(){
-        console.log(props.data.value)
+export default function Task(props: any){
+
+    const [title, setTitle] = useState(props.data.title)
+    const [value, setValue] = useState(props.data.value)
+
+    function editTitle(title: String){
+        setTitle(title)
+        props.editTask(props.data.id, value, title)
     }
+
+    function editTask(value: Boolean){
+        setValue(value)
+        props.editTask(props.data.id, value, title)
+    }
+
     return(
         <div className="task">
-            <input type="checkbox" value={props.data.value}/>
-            <div className="infomations" onClick={oi}>
-                <h4>{props.data.title}</h4>
-                <p>{props.data.description}</p>
+            <input type="checkbox" defaultChecked={value} value={value} onChange = { () => editTask(!value) }/>
+            <div className="infomations">
+                <input value={title} onChange = {e => editTitle(e.target.value) }/>
+                <button onClick={()=>props.deleteTask(props.data.id)}>Delete</button>
             </div>
         </div>
     )
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,38 +1,103 @@
 import React, { useState } from 'react'
-import Checklist from '../../components/checklist/checklist'
+import Task from '../../components/task/task'
 import './styles.css'
+
+interface tasks {
+    id: number,
+    value: Boolean,
+    title: String,
+}
+
 
 export default function Home(){
 
-    const [title, setTitle] = useState()
+    let [title, setTitle] = useState('')
+    let [tasks, setTasks] = useState<tasks[]>([])//JSON.parse(localStorage.getItem("checklists")) ;
+    let [visibleTasks, setVisibleTasks] = useState<tasks[]>(tasks)
+    let [filter, setFilter] = useState('Default')
+    let [searchTitle, setSearchTitle] = useState('')
+    let [count, setCount] = useState(0)
 
-    const checklists = [
-        {
-            title: "CheckList1", 
-            tasks: []
-        },
+    
+    function addTask(){
+        setCount(()=> count++)
+        const newItem = {id: count, value: false, title: title}
+        setTasks(prev => [...prev.slice(0, tasks.length+1), newItem, ...prev.slice(tasks.length+1)])
+        setVisibleTasks(prev => [...prev.slice(0, tasks.length+1), newItem, ...prev.slice(tasks.length+1)])
+        localStorage.setItem("task",  JSON.stringify(tasks))
+    }
+    
+    function deleteTask(id: Number){
+        setTasks(tasks.filter(task => task.id !== id))
+        setVisibleTasks(visibleTasks.filter(task => task.id !== id))
+        localStorage.setItem("task",  JSON.stringify(tasks))
+    }
+    
+    function editTask(id: number, value: boolean, title: String){
+        const aux = tasks
+        console.log(title)
+        aux.map( task => { if(task.id === id){task.title = title; task.value = value}})
+        setTasks(()=>aux)
+        localStorage.setItem("task",  JSON.stringify(tasks))
+    }
+
+    function filterTasks(value: string, searchTitle: string){
+        if(value === 'All'){
+            setVisibleTasks(()=> tasks)
+        }
+        if(value === 'Complete'){
+            setVisibleTasks(tasks.filter(task => task.value === true))
+        }
+        if(value === "Incomplete"){
+            setVisibleTasks(tasks.filter(task => task.value === false))
+        }
+        if(searchTitle !== ''){
+            if(value === 'All')
+            setVisibleTasks(tasks.filter(task => task.title === searchTitle))
+            else
+            setVisibleTasks(visibleTasks.filter(task => task.title === searchTitle))
+        }
+
         
-    ]
+
+        setFilter(value)
+        setSearchTitle(searchTitle)
+    }
+
     return(
         <main>
             <div className="boardTitle">
                 <div>
                     <h2>Title Table</h2>
                     <div>
-                        <h3>Filter:</h3>
-                        <select name="select">
-                            <option value="valor1">Valor 1</option>
-                            <option value="valor2" selected>Valor 2</option>
-                            <option value="valor3">Valor 3</option>
-                        </select>
+                        <div>
+                            <input onChange={ e =>filterTasks(filter, e.target.value) }/>
+                        </div>
+                        <div className="Filter">
+                            <h3>Filter:</h3>
+                            <select name="select" onChange = { e =>filterTasks(e.target.value, searchTitle) } >
+                                <option value="All">All</option>
+                                <option value="Complete">Completed Tasks</option>
+                                <option value="Incomplete">Uncompleted Tasks</option>
+                            </select>
+                        </div>
                     </div>
                 </div>
             </div>
-            <div className="tables">
-                <div className="CheckListTables">
-                    {checklists.map(checklist =><Checklist title={checklist.title} tasks={checklist.tasks}/>)}
+
+            <div className="checklist">
+                <div className="taskList">
+                    {visibleTasks.map(a=>(
+                        <div key={a.id}>
+                            <Task data= {a} deleteTask={ deleteTask } editTask={ editTask }/>
+                        </div>
+                    ))
+                    }
                 </div>
-                <button className="addChicklist">Add checklist</button>
+                <div className="addTask">
+                    <input placeholder="Add Task" onChange = {e => setTitle(e.target.value)}/>
+                    <button onClick={ addTask }>ADD</button>
+                </div>
             </div>
         </main>
     )
